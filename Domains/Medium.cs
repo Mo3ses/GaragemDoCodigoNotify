9ed51b363950338478f11a50586e7f31dfc1d1d2
Brenno-Ribeiro <brennodevelop100@.gmailcom>
2022-04-06 17:02:40 -0300

    Criando a classe de response do medium

---
+++ Domains/Medium.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Medium
+{
+    public Guid Guid { get; set; }
+    public string Title { get; set; }
+    public DateTime PubDate { get; set; }
+    public string Link { get; set; }
+    public string Author { get; set; }
+    public string Thumbnail { get; set; }
+    public string Description { get; set; }
+    public string Content { get; set; }
+    public IList<string> Categories { get; set; }
+}
